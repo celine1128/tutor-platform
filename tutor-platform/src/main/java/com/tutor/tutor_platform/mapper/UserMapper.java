56189cb66d0512da3653bdcae6d304b344f1a1b0
yang-qin-a <2557592237@qq.com>
2026-05-13 00:32:23 +0800

    删除冗余文件

--- tutor-platform/src/main/java/com/tutor/tutor_platform/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.tutor.tutor_platform.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.tutor.tutor_platform.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper extends BaseMapper<User> {
-}
